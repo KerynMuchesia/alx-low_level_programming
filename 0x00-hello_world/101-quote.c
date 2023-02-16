6d4577d97085b674a3979d5ed1e42e4d0eb783cb
KerynMuchesia <kerynmuchesia1@gmail.com>
2023-02-16 05:21:42 -0800

    alx-low_level_programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,17 @@
-#include <stdio.h>
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
- * Return: Always 1 (Success)
+ *
+ * Return: Always 1 (Error code)
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			/" - Dora Korpar, 2015-10-19\n", 59);
+	char str[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	ssize_t len = sizeof(str) - 1;
+
+	if (write(STDERR_FILENO, str, len) != len)
+		return (1);
+
 	return (1);
 }
