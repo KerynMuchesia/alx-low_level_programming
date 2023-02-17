bb9dce4d6f469e7257960d9aa413c3bae755d9f1
KerynMuchesia <kerynmuchesia1@gmail.com>
2023-02-17 07:07:30 -0800

    0x01-variables_if_else_while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char x;
+	x = 'a';
+	for
+		(x <= 'a'){
+		putchar(x);
+		c++;
+		}
+	putchar('\n');
+	return (0)
+}
