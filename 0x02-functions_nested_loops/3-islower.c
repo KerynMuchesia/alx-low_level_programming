d9e9a50f86197905f19c182ff34072b2ae7c856c
KerynMuchesia <kerynmuchesia1@gmail.com>
2023-02-21 06:15:18 -0800

    mine

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- *  a function that checks for lowercase character
+ *  a: function that checks for lowercase character
  *  Return: 0 (Success)
  */
 
 int _islower(int c)
 {
-	if (c = 'a'; && c <= 'z')
+	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,9 +1,9 @@
 #ifndef main_h
 #define main_h
 
-int putchar(int c);
+int _putchar(int c);
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
 
-#endif /* main_h */
+#endif
