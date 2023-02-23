1147ae845bbd4ff5d2460a2d2ccc60b31e65f92d
KerynMuchesia <kerynmuchesia1@gmail.com>
2023-02-23 02:27:12 -0800

    mine

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(int c)
+{
+    char c;
+
+    if (c >= 'A' && c <= 'Z)
+    {
+	    return(1);
+    }
+    else (c >= 'a' && c <= 'z')
+    {
+	    return (0);
+    }
+}
