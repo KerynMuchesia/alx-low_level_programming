fc52813c6bf5ceb8ef93761fe3e79a5a6089f756
KerynMuchesia <kerynmuchesia1@gmail.com>
2023-02-23 04:31:33 -0800

    mine

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *  print_numbers - checks for checks for a digit (0 through 9).
+ * print_numbers - checks for checks for a digit (0 through 9).
  *
  * Return: Always 0.
  */
@@ -10,7 +10,7 @@ void print_numbers(void)
 {
 	int k;
 
-	for (k = 0; k = 10; k++)
+	for (k = 0; k < 10; k++)
 	{
 		_putchar(k);
 	}
