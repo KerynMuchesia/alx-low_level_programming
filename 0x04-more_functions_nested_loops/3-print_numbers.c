9787178a30ffb16ea0beda8b33df8ea421d992d4
KerynMuchesia <kerynmuchesia1@gmail.com>
2023-02-23 04:12:10 -0800

    mine

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.c"
+
+/**
+ *  print_numbers - checks for checks for a digit (0 through 9).
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+	int k;
+	if (k >= '0'; k <= '9'; k++)
+	{
+		_putchar("k");
+	}
+	-putchar("\n");
+}
