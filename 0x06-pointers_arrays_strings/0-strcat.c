a5c9115d7be44ca2fb038931a816d2c69a29cc7e
KerynMuchesia <kerynmuchesia1@gmail.com>
2023-03-01 00:30:40 -0800

    pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include <main.h>
+
+/**
+ * _strcat - a function that concatenates two strings
+ * @src: string
+ * @dest: string
+ *
+ * Return:
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	char *end = dest;
+	while (*end != '\0')
+	{
+		end++;
+	}
+	while (*src != '\0')
+	{
+		*end++ = *src++;
+	}
+	*end = '\0'
+		return dest;
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#IFNDEF "MAIN_C"
+#DEFINE "MAIN_C"
+char *_strcat(char *dest, char *src);
+
+#ENDIF
