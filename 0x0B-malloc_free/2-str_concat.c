872369d356ffd0c0ba36366d4cd40f2e7e904d8e
KerynMuchesia <kerynmuchesia1@gmail.com>
2023-03-14 09:14:51 -0700

    yes

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,84 +1,30 @@
-#include "main.h"
-		
+#include <stdio.h>
 #include <stdlib.h>
-		
+#include <string.h>
+
 /**
-		
- * str_concat - get ends of input and add together for size
-		
- * @s1: input one to concat
-		
- * @s2: input two to concat
-		
- * Return: concat of s1 and s2
-		
+ * str_concat - function that concatenates two strings.
+ *
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: NULL on failure
  */
-		
+
 char *str_concat(char *s1, char *s2)
-		
 {
-		
-	char *conct;
-		
-	int i, ci;
-		
-
-		
-	if (s1 == NULL)
-		
-		s1 = "";
-		
-	if (s2 == NULL)
-		
-		s2 = "";
-		
-
-		
-		i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-		i++;
-		
-	while (s2[ci] != '\0')
-		
-		ci++;
-		
-	conct = malloc(sizeof(char) * (i + ci + 1));
-		
-
-		
-	if (conct == NULL)
-		
-		return (NULL);
-		
-	i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-	{
-		
-		conct[i] = s1[i];
-		
-		i++;
-		
-	}
-		
-
-		
-	while (s2[ci] != '\0')
-		
-	{
-		
-		conct[i] = s2[ci];
-		
-		i++, ci++;
-		
-	}
-		
-	conct[i] = '\0';
-		
-	return (conct);
-		
+	if (s1 == NULL) s1 = "";
+	if (s2 == NULL) s2 = "";
+	
+	size_t len1 = strlen(s1);
+	size_t len2 = strlen(s2);
+	
+	char *result = (char*) malloc((len1 + len2 + 1) * sizeof(char));
+	
+	if (result == NULL) return NULL;
+	
+	memcpy(result, s1, len1);
+	memcpy(result + len1, s2, len2 + 1);
+	
+	return result;
 }
-
