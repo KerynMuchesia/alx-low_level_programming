77928123bb33b6e6b514c5d286483545df4cd715
KerynMuchesia <kerynmuchesia1@gmail.com>
2023-04-09 05:49:46 -0700

    file io

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
  * read_textfile - reads a text file and prints it to the POSIX standard output
@@ -10,42 +11,32 @@
  *         0 if filename is NULL
  *         0 if write fails or does not write the expected amount of bytes
  */
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-        int fd;
-        ssize_t num_read, num_written;
-        char *buf;
+	 ssize_t a, b, c;
+	char *buffer;
 
-        if (!filename)
-                return (0);
+	if (filename == NULL)
+		return (0);
 
-        fd = open(filename, O_RDONLY);
-        if (fd == -1)
-                return (0);
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer ==  NULL)
+		return (0);
 
-        buf = malloc(sizeof(char) * (letters + 1));
-        if (!buf)
-        {
-                close(fd);
-                return (0);
-        }
+	a = open(filename, O_RDONLY);
+	b = read(o, buffer, letters);
+	c = write(STDOUT_FILENO, buffer, b);
 
-        num_read = read(fd, buf, letters);
-        close(fd);
+	if (a == -1 || b == -1 || c == -1 || c != b)
+	{
+		free(buffer);
+		return (0);
+	}
 
-        if (num_read == -1)
-        {
-                free(buf);
-                return (0);
-        }
+	free(buffer);
+	close(o);
 
-        buf[num_read] = '\0';
+	return (c);
 
-        num_written = write(STDOUT_FILENO, buf, num_read);
-        free(buf);
-
-        if (num_written != num_read)
-                return (0);
-
-        return (num_written);
 }
